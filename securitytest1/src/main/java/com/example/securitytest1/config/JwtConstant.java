5a8fa95b94378c94e0f97ba9a54f89150ad55ec3
CHANDAN CHAUHAN <125190359+chandanUP54@users.noreply.github.com>
2023-12-18 20:27:15 +0530

    Delete securitytest1 directory

--- securitytest1/src/main/java/com/example/securitytest1/config/JwtConstant.java
+++
@@ -1,8 +0,0 @@
-package com.example.securitytest1.config;
-
-public class JwtConstant {
-
-	public static final String SECRET_KEY="fdhsosanshjaiwieuuwibcbhdehdiwiecbbcnhdsdhidw";
-	public static final String  JWT_HEADER="Authorization";
-	
-}
